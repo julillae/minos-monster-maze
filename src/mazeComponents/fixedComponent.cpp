c9ce42ef49c3f65c3ccaad5d3a5b8c4e818bc3ea
Henry <henrywwdeng@gmail.com>
2019-01-31 12:19:06 -0800

    changed collision implementation for floors

--- src/mazeComponents/fixedComponent.cpp
+++ src/mazeComponents/fixedComponent.cpp
@@ -1,7 +1,3 @@
 #include "../include/mazeComponents/fixedComponent.hpp"
 
 // Put implementation specific to fixed maze components here
-
-vec2 FixedComponent::get_bounding_box()const {
-    // TODO: implement bounding box method
-}
