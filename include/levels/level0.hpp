60fc9854a88a571e5e0f881147d6773b2d174ae3
Shanice M <shanicemcsavaney@gmail.com>
2019-02-01 11:06:57 -0800

    Generate maze according to hard-coded array

--- include/levels/level0.hpp
+++ include/levels/level0.hpp
@@ -46,6 +46,10 @@ private:
 	void on_key(GLFWwindow*, int key, int, int action, int mod);
 	void on_mouse_move(GLFWwindow* window, double xpos, double ypos);
 
+	// Generates a new floor
+	bool spawn_floor();
+
+	// Generates hard-coded maze in each level
 	void generate_maze();
 private:
 	// Window handle
