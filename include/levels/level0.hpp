45e00803f6cb6ef4ee384346d8a388bca8762449
Shanice M <shanicemcsavaney@gmail.com>
2019-02-01 00:10:03 -0800

    Add level for skeletal game

---
+++ include/levels/level0.hpp
@@ -0,0 +1,76 @@
+#pragma once
+
+// internal
+#include "../common.hpp"
+#include "../characters/player.hpp"
+#include "../characters/salmon.hpp"
+#include "../characters/turtle.hpp"
+#include "../characters/fish.hpp"
+#include "../mazeComponents/mazeComponent.hpp"
+#include "../mazeComponents/fixedComponent.hpp"
+#include "../mazeComponents/floor.hpp"
+#include "../mazeComponents/ice.hpp"
+#include "../water.hpp"
+
+// stlib
+#include <vector>
+#include <random>
+
+#define SDL_MAIN_HANDLED
+#include <SDL.h>
+#include <SDL_mixer.h>
+
+// One of our many worlds
+
+class Level0
+{
+public:
+	Level0();
+	~Level0();
+
+    // Creates a window, sets up events and begins the game
+	bool init(vec2 screen);
+
+    void destroy();
+
+    bool update(float elapsed_ms);
+
+    // Renders our scene
+	void draw();
+
+	// Should the game be over ?
+	bool is_over()const;
+
+private:
+	// !!! INPUT CALLBACK FUNCTIONS
+	void on_key(GLFWwindow*, int key, int, int action, int mod);
+	void on_mouse_move(GLFWwindow* window, double xpos, double ypos);
+
+private:
+	// Window handle
+	GLFWwindow* m_window;
+
+    // Screen texture
+	// The draw loop first renders to this texture, then it is used for the water shader
+	GLuint m_frame_buffer;
+	Texture m_screen_tex;
+
+	// Water effect
+	Water m_water;
+
+    Player m_player;
+    std::vector<Floor> m_floor;
+
+    float m_current_speed;
+
+    float m_next_floor_spawn;
+
+    Mix_Music* m_background_music;
+
+    // C++ rng
+	std::default_random_engine m_rng;
+	std::uniform_real_distribution<float> m_dist; // default 0..1
+
+    // initial position of player
+	vec2 initialPosition = { 200.f, 500.f };
+};
---
+++ src/levels/level0.cpp
@@ -0,0 +1,287 @@
+// Header
+#include "../include/levels/level0.hpp"
+
+#include "../include/physics.hpp"
+
+// stlib
+#include <string.h>
+#include <cassert>
+#include <sstream>
+
+namespace
+{
+	// Constants go here
+
+	namespace
+	{
+		void glfw_err_cb(int error, const char* desc)
+		{
+			fprintf(stderr, "%d: %s", error, desc);
+		}
+	}
+}
+
+Level0::Level0() : 
+	m_next_floor_spawn(0.f)
+{
+	// Seeding rng with random device
+	m_rng = std::default_random_engine(std::random_device()());
+}
+
+Level0::~Level0()
+{
+
+}
+
+// Level0 initialization
+bool Level0::init(vec2 screen)
+{
+	//-------------------------------------------------------------------------
+	// GLFW / OGL Initialization
+	// Core Opengl 3.
+	glfwSetErrorCallback(glfw_err_cb);
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW");
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, 1);
+#if __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+#endif
+	glfwWindowHint(GLFW_RESIZABLE, 0);
+	m_window = glfwCreateWindow((int)screen.x, (int)screen.y, "A1 Assignment", nullptr, nullptr);
+	if (m_window == nullptr)
+		return false;
+
+	glfwMakeContextCurrent(m_window);
+	glfwSwapInterval(1); // vsync
+
+	// Load OpenGL function pointers
+	gl3w_init();
+
+	// Setting callbacks to member functions (that's why the redirect is needed)
+	// Input is handled using GLFW, for more info see
+	// http://www.glfw.org/docs/latest/input_guide.html
+	glfwSetWindowUserPointer(m_window, this);
+	auto key_redirect = [](GLFWwindow* wnd, int _0, int _1, int _2, int _3) { ((Level0*)glfwGetWindowUserPointer(wnd))->on_key(wnd, _0, _1, _2, _3); };
+	auto cursor_pos_redirect = [](GLFWwindow* wnd, double _0, double _1) { ((Level0*)glfwGetWindowUserPointer(wnd))->on_mouse_move(wnd, _0, _1); };
+	glfwSetKeyCallback(m_window, key_redirect);
+	glfwSetCursorPosCallback(m_window, cursor_pos_redirect);
+
+	// Create a frame buffer
+	m_frame_buffer = 0;
+	glGenFramebuffers(1, &m_frame_buffer);
+	glBindFramebuffer(GL_FRAMEBUFFER, m_frame_buffer);
+
+	// Initialize the screen texture
+	m_screen_tex.create_from_screen(m_window);
+
+	//-------------------------------------------------------------------------
+	// Loading music and sounds
+	if (SDL_Init(SDL_INIT_AUDIO) < 0)
+	{
+		fprintf(stderr, "Failed to initialize SDL Audio");
+		return false;
+	}
+
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048) == -1)
+	{
+		fprintf(stderr, "Failed to open audio device");
+		return false;
+	}
+
+	m_background_music = Mix_LoadMUS(audio_path("music.wav"));
+
+	if (m_background_music == nullptr)
+	{
+		fprintf(stderr, "Failed to load sounds\n %s\n %s\n %s\n make sure the data directory is present",
+			audio_path("music.wav"),
+			audio_path("salmon_dead.wav"),
+			audio_path("salmon_eat.wav"));
+		return false;
+	}
+
+	// Playing background music undefinitely
+	Mix_PlayMusic(m_background_music, -1);
+	
+	fprintf(stderr, "Loaded music\n");
+
+	m_current_speed = 1.f;
+	
+	return m_water.init() && m_player.init(initialPosition);
+}
+
+// Releases all the associated resources
+void Level0::destroy()
+{
+	glDeleteFramebuffers(1, &m_frame_buffer);
+
+	if (m_background_music != nullptr)
+		Mix_FreeMusic(m_background_music);
+
+	Mix_CloseAudio();
+
+	m_player.destroy();
+
+	glfwDestroyWindow(m_window);
+}
+
+// Update our game world
+bool Level0::update(float elapsed_ms)
+{
+	int w, h;
+        glfwGetFramebufferSize(m_window, &w, &h);
+	vec2 screen = { (float)w, (float)h };
+
+	Physics *physicsHandler = new Physics();
+
+	// TODO: Check for Player-Platform Collisions
+
+	for (const auto& floor: m_floor)
+	{
+		if (physicsHandler->collisionWithFixedWalls(&m_player, &floor).isCollided)
+		{
+			// do something
+		}
+	}
+
+	// Updating all entities
+
+	m_player.update(elapsed_ms);
+
+	m_next_floor_spawn -= elapsed_ms * m_current_speed;
+	// if (m_floor.size() <= MAX_TURTLES && m_next_floor_spawn < 0.f)
+	// {
+	// 	if (!spawn_floor())
+	// 		return false;
+
+	// 	MazeComponent& new_floor = m_floor.back();	
+
+	// 	// Setting random initial position
+	// 	new_floor.set_position({ screen.x - 150, 50 + m_dist(m_rng) * (screen.y - 100) });
+
+	// 	// Next spawn
+	// 	m_next_floor_spawn = (TURTLE_DELAY_MS / 2) + m_dist(m_rng) * (TURTLE_DELAY_MS/2);
+	// }
+
+	// If player is dead, restart the game after the fading animation
+	if (!m_player.is_alive()) {
+		int w, h;
+		glfwGetWindowSize(m_window, &w, &h);
+		m_current_speed = 1.f;
+	}
+
+	return true;
+}
+
+// Render our game world
+// http://www.opengl-tutorial.org/intermediate-tutorials/tutorial-14-render-to-texture/
+void Level0::draw()
+{
+	// Clearing error buffer
+	gl_flush_errors();
+
+	// Getting size of window
+	int w, h;
+    glfwGetFramebufferSize(m_window, &w, &h);
+
+	// Updating window title with points
+	std::stringstream title_ss;
+	title_ss << "Minos' Monster Maze";
+	glfwSetWindowTitle(m_window, title_ss.str().c_str());
+
+	/////////////////////////////////////
+	// First render to the custom framebuffer
+	glBindFramebuffer(GL_FRAMEBUFFER, m_frame_buffer);
+
+	// Clearing backbuffer
+	glViewport(0, 0, w, h);
+	glDepthRange(0.00001, 10);
+	const float clear_color[3] = { 0.3f, 0.3f, 0.8f };
+	glClearColor(clear_color[0], clear_color[1], clear_color[2], 1.0);
+	glClearDepth(1.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Fake projection matrix, scales with respect to window coordinates
+	// PS: 1.f / w in [1][1] is correct.. do you know why ? (:
+	float left = 0.f;// *-0.5;
+	float top = 0.f;// (float)h * -0.5;
+	float right = (float)w;// *0.5;
+	float bottom = (float)h;// *0.5;
+
+	float sx = 2.f / (right - left);
+	float sy = 2.f / (top - bottom);
+	float tx = -(right + left) / (right - left);
+	float ty = -(top + bottom) / (top - bottom);
+	mat3 projection_2D{ { sx, 0.f, 0.f },{ 0.f, sy, 0.f },{ tx, ty, 1.f } };
+
+	for (auto& floor : m_floor)
+		floor.draw(projection_2D);	
+	m_player.draw(projection_2D);
+
+	/////////////////////
+	// Truely render to the screen
+	glBindFramebuffer(GL_FRAMEBUFFER, 0);
+
+	// Clearing backbuffer
+	glViewport(0, 0, w, h);
+	glDepthRange(0, 10);
+	glClearColor(0, 0, 0, 1.0);
+	glClearDepth(1.f);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+	// Bind our texture in Texture Unit 0
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, m_screen_tex.id);
+
+	m_water.draw(projection_2D);
+
+	//////////////////
+	// Presenting
+	glfwSwapBuffers(m_window);
+}
+
+// Should the game be over ?
+bool Level0::is_over()const
+{
+	return glfwWindowShouldClose(m_window);
+}
+
+// On key callback
+void Level0::on_key(GLFWwindow*, int key, int, int action, int mod)
+{
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+	// key is of 'type' GLFW_KEY_
+	// action can be GLFW_PRESS GLFW_RELEASE GLFW_REPEAT
+	// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+
+	m_player.set_direction(key, action);
+
+	// Resetting game
+	if (action == GLFW_RELEASE && key == GLFW_KEY_R)
+	{
+		int w, h;
+		glfwGetWindowSize(m_window, &w, &h);
+		m_player.destroy();
+		m_player.init(initialPosition);
+		m_current_speed = 1.f;
+	}
+
+	// Control the current speed with `<` `>`
+	if (action == GLFW_RELEASE && (mod & GLFW_MOD_SHIFT) &&  key == GLFW_KEY_COMMA)
+		m_current_speed -= 0.1f;
+	if (action == GLFW_RELEASE && (mod & GLFW_MOD_SHIFT) && key == GLFW_KEY_PERIOD)
+		m_current_speed += 0.1f;
+	
+	m_current_speed = fmax(0.f, m_current_speed);
+}
+
+void Level0::on_mouse_move(GLFWwindow* window, double xpos, double ypos)
+{
+
+}
