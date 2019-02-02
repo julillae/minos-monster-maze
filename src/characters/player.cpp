dc9e2ba7f87cb96e92faea8a1f5c493dcb5032dc
Yuntian Wan <yuntianwan@dhcp-206-87-157-212.ubcsecure.wireless.ubc.ca>
2019-02-01 18:12:23 -0800

    a box for player

--- src/characters/player.cpp
+++ src/characters/player.cpp
@@ -8,46 +8,73 @@
 #include <string>
 #include <algorithm>
 #include <math.h>
+#include <cmath>
+
+Texture Player::fish_texture;
 
 bool Player::init()
 {
-	std::vector<Vertex> vertices;
-	std::vector<uint16_t> indices;
-
+	
 	// Reads the salmon mesh from a file, which contains a list of vertices and indices
-	FILE* mesh_file = fopen(mesh_path("salmon.mesh"), "r");
-	if (mesh_file == nullptr)
-		return false;
+	//FILE* mesh_file = fopen(mesh_path("salmon.mesh"), "r");
+	//if (mesh_file == nullptr)
+	//	return false;
 
 	// Reading vertices and colors
-	size_t num_vertices;
-	fscanf(mesh_file, "%zu\n", &num_vertices);
-	for (size_t i = 0; i < num_vertices; ++i)
-	{
-		float x, y, z;
-		float _u[3]; // unused
-		int r, g, b;
-		fscanf(mesh_file, "%f %f %f %f %f %f %d %d %d\n", &x, &y, &z, _u, _u+1, _u+2, &r, &g, &b);
-		Vertex vertex;
-		vertex.position = { x, y, -z }; 
-		vertex.color = { (float)r / 255, (float)g / 255, (float)b / 255 };
-		vertices.push_back(vertex);
-	}
+	//size_t num_vertices;
+	//fscanf(mesh_file, "%zu\n", &num_vertices);
+	//for (size_t i = 0; i < num_vertices; ++i)
+	//{
+		//float x, y, z;
+		//float _u[3]; // unused
+		//int r, g, b;
+		//fscanf(mesh_file, "%f %f %f %f %f %f %d %d %d\n", &x, &y, &z, _u, _u+1, _u+2, &r, &g, &b);
+		//Vertex vertex;
+		//vertex.position = { x, y, -z }; 
+		//vertex.color = { (float)r / 255, (float)g / 255, (float)b / 255 };
+		//vertices.push_back(vertex);
+	//}
 
 	// Reading associated indices
-	size_t num_indices;
-	fscanf(mesh_file, "%zu\n", &num_indices);
-	for (size_t i = 0; i < num_indices; ++i)
-	{
-		int idx[3];
-		fscanf(mesh_file, "%d %d %d\n", idx, idx + 1, idx + 2);
-		indices.push_back((uint16_t)idx[0]);
-		indices.push_back((uint16_t)idx[1]);
-		indices.push_back((uint16_t)idx[2]);
-	}
+	//size_t num_indices;
+	//fscanf(mesh_file, "%zu\n", &num_indices);
+	//for (size_t i = 0; i < num_indices; ++i)
+	//{
+		//int idx[3];
+		//fscanf(mesh_file, "%d %d %d\n", idx, idx + 1, idx + 2);
+		//indices.push_back((uint16_t)idx[0]);
+		//indices.push_back((uint16_t)idx[1]);
+		//indices.push_back((uint16_t)idx[2]);
+	//}
 
 	// Done reading
-	fclose(mesh_file);
+	//fclose(mesh_file);
+
+	if (!fish_texture.is_valid())
+	{
+		if (!fish_texture.load_from_file(textures_path("fish.png")))
+		{
+			fprintf(stderr, "Failed to load turtle texture!");
+			return false;
+		}
+	}
+
+	// The position corresponds to the center of the texture
+	float wr = fish_texture.width * 0.5f;
+	float hr = fish_texture.height * 0.5f;
+
+	TexturedVertex vertices[4];
+	vertices[0].position = { -wr, +hr, -0.01f };
+	vertices[0].texcoord = { 0.f, 1.f };
+	vertices[1].position = { +wr, +hr, -0.01f };
+	vertices[1].texcoord = { 1.f, 1.f,  };
+	vertices[2].position = { +wr, -hr, -0.01f };
+	vertices[2].texcoord = { 1.f, 0.f };
+	vertices[3].position = { -wr, -hr, -0.01f };
+	vertices[3].texcoord = { 0.f, 0.f };
+
+	// counterclockwise as it's the default opengl front winding direction
+	uint16_t indices[] = { 0, 3, 1, 1, 3, 2 };
 
 	// Clearing errors
 	gl_flush_errors();
@@ -55,12 +82,12 @@ bool Player::init()
 	// Vertex Buffer creation
 	glGenBuffers(1, &mesh.vbo);
 	glBindBuffer(GL_ARRAY_BUFFER, mesh.vbo);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * vertices.size(), vertices.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(Vertex) * 4, vertices, GL_STATIC_DRAW);
 
 	// Index Buffer creation
 	glGenBuffers(1, &mesh.ibo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, mesh.ibo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint16_t) * indices.size(), indices.data(), GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint16_t) * 6, indices, GL_STATIC_DRAW);
 
 	// Vertex Array (Container for Vertex + Index buffer)
 	glGenVertexArrays(1, &mesh.vao);
@@ -68,15 +95,13 @@ bool Player::init()
 		return false;
 
 	// Loading shaders
-	if (!effect.load_from_file(shader_path("colored.vs.glsl"), shader_path("colored.fs.glsl")))
+	if (!effect.load_from_file(shader_path("textured.vs.glsl"), shader_path("textured.fs.glsl")))
 		return false;
 	
 	// Setting initial values
-	m_scale.x = -35.f;
-	m_scale.y = 35.f;
+	m_scale.x = -0.4f;
+	m_scale.y = 0.4f;
 	m_is_alive = true;
-	m_num_indices = indices.size();
-	h_direction = Direction::none;
 	v_direction = Direction::none;
 	m_position = { 150.f, 200.f };
 	m_rotation = 0.f;
@@ -135,7 +160,6 @@ void Player::draw(const mat3& projection)
 	transform_translate(m_position);
 	transform_scale(m_scale);
 	transform_rotate(m_rotation);
-
 	transform_end();
 
 	// Setting shaders
@@ -158,26 +182,37 @@ void Player::draw(const mat3& projection)
 
 	// Input data location as in the vertex buffer
 	GLint in_position_loc = glGetAttribLocation(effect.program, "in_position");
-	GLint in_color_loc = glGetAttribLocation(effect.program, "in_color");
+	GLint in_texcoord_loc = glGetAttribLocation(effect.program, "in_texcoord");
 	glEnableVertexAttribArray(in_position_loc);
-	glEnableVertexAttribArray(in_color_loc);
-	glVertexAttribPointer(in_position_loc, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)0);
-	glVertexAttribPointer(in_color_loc, 3, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)sizeof(vec3));
+	glEnableVertexAttribArray(in_texcoord_loc);
+	glVertexAttribPointer(in_position_loc, 3, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void*)0);
+	glVertexAttribPointer(in_texcoord_loc, 3, GL_FLOAT, GL_FALSE, sizeof(TexturedVertex), (void*)sizeof(vec3));
+
+	// Enabling and binding texture to slot 0
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, fish_texture.id);
 
 	// Setting uniform values to the currently bound program
 	glUniformMatrix3fv(transform_uloc, 1, GL_FALSE, (float*)&transform);
 
 	// !!! Salmon Color
-	float color[] = { 1.f, 0.f, 0.0f };
-	if (!m_is_alive) {
-		color[1] = 0.f;
-		color[2] = 0.f;
-	}
+	float color[] = { 1.f, 1.f, 1.f };
+	//if (!m_is_alive) {
+	//	color[1] = 0.f;
+	//	color[2] = 0.f;
+	//}
 	glUniform3fv(color_uloc, 1, color);
 	glUniformMatrix3fv(projection_uloc, 1, GL_FALSE, (float*)&projection);
 
 	// Drawing!
 	glDrawElements(GL_TRIANGLES,(GLsizei)m_num_indices, GL_UNSIGNED_SHORT, nullptr);
+}
+
+// Returns the local bounding coordinates scaled by the current size of the fish 
+vec2 Player::get_bounding_box()const
+{
+	// fabs is to avoid negative scale due to the facing direction
+	return { std::fabs(m_scale.x) * fish_texture.width, std::fabs(m_scale.y) * fish_texture.height };
 }
 
 // Simple bounding box collision check, 
