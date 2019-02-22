a934eb35c3cbe08e6810efec858c0fd2c61aa2aa
Shanice M <shanicemcsavaney@gmail.com>
2019-02-22 09:12:13 -0800

    #15 TA fix for maze rendering problem

--- src/common.cpp
+++ src/common.cpp
@@ -248,9 +248,13 @@ bool Effect::load_from_file(const char* vs_path, const char* fs_path)
 
 void Effect::release()
 {
-	glDeleteProgram(program);
-	glDeleteShader(vertex);
-	glDeleteShader(fragment);
+	glDetachShader(program, vertex);
+  	glDeleteShader(vertex);
+
+  	glDetachShader(program, fragment);
+  	glDeleteShader(fragment);
+
+  	glDeleteProgram(program);
 }
 
 void Renderable::transform_begin()
