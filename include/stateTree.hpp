6624c1446b3409b7e03a5494b556c9cbb42567b5
Julilla Paul <julillapaul@gmail.com>
2019-03-27 13:11:26 -0700

    Minotaur will follow you when you get within a certain range #124

--- include/stateTree.hpp
+++ include/stateTree.hpp
@@ -6,7 +6,7 @@ using namespace std;
 
 // Enum for character states
 // set StateMAX = last item in enum, needed to know how many states are available
-enum State { idle, running, jumping, rising, falling, landing, frozen, thawing, reviving, dead, StateMAX = dead};
+enum State { idle, running, jumping, rising, falling, landing, frozen, thawing, reviving, following, dead, StateMAX = dead};
 
 struct Edge {
 	State src;
--- src/characters/minotaur.cpp
+++ src/characters/minotaur.cpp
@@ -4,6 +4,8 @@
 #include "../include/common.hpp"
 #include "../include/physics.hpp"
 #include "../include/gameAI.hpp"
+#include "../include/level.hpp"
+#include "../include/a1.hpp"
 
 // Put implementation for Minotaur enemy here
 
@@ -28,7 +30,7 @@ bool Minotaur::init(vec2 initialPosition, Physics * physicsHandler)
 
 	spriteSheet.set_render_data(this, 0);
 
-	initStateTree();
+	initStateTreeMinotaur();
 	set_properties(initialPosition, 3.0f, speed);
 	m_frozen = false;
 	set_dimensions(&minotaur_texture, spriteSheetWidth, spriteSheetHeight, horizontalTrim, verticalTrim);
@@ -39,27 +41,98 @@ bool Minotaur::init(vec2 initialPosition, Physics * physicsHandler)
 
 void Minotaur::update(float ms)
 {
-	if (is_alive() && !m_frozen)
+	if (is_alive() && !m_frozen && !isPlayerClose())
 	{
         // Want to move in direction of player if within a certain distance. Will stop at stop_bound if player still within range, be idle
         // Also want to be able to rotate world?
-        
-        // Update enemy position based on fixed path here
-		float vel_x = m_velocity.x;
-		float vel_y = m_velocity.y;
-		float right_bound = m_initialPosition.x + m_stopBound;
-		float left_bound = m_initialPosition.x;
-		float next_pos_x = m_position.x + vel_x;
-
-		if (((vel_x > 0) && (next_pos_x >= right_bound)) ||
-			((vel_x < 0) && (next_pos_x <= left_bound))) {
-			vel_x *= -1;
-			m_scale.x *= -1;
-			set_velocity({ vel_x, vel_y });
+        characterState->changeState(running);
+		if (atBound()) {
+            printf("Pacing: switched direction");
+            if (direction == Direction::left) {
+                direction = Direction::right;
+            } else {
+                direction = Direction::left;
+            }
 		}
-		move();
-	}
+	} else if (is_alive() && !m_frozen && isPlayerClose())
+    {
+        vec2 playerLoc = world.m_player.get_position();
+        if (atBound())
+        {
+            characterState->changeState(idle);
+        } else if (abs(playerLoc.x - m_position.x) <= 10.f){
+            characterState->changeState(idle);
+        } else
+        {
+            setFollowDirection();
+            characterState->changeState(following);
+        }        
+    }
+    updateVelocity();
+    move();
 
+}
+
+void Minotaur::updateVelocity()
+{
+    switch (characterState->currentState){
+        case idle:
+            m_velocity.x = 0.f;
+            break;
+        case running:
+            if (direction == Direction::right) {
+                m_velocity.x = maxHorzSpeed;
+            } else {
+                m_velocity.x = maxHorzSpeed * -1;
+            }
+            break;
+        case following:
+            if (direction == Direction::right) {
+                m_velocity.x = maxHorzSpeed + 1.f;
+            } else {
+                m_velocity.x = (maxHorzSpeed + 1.f)* -1;
+            }
+            break;
+        default:
+            break;
+    }
+}
+
+bool Minotaur::atBound()
+{
+    float vel_x = m_velocity.x;
+    float vel_y = m_velocity.y;
+    float right_bound = m_initialPosition.x + m_stopBound;
+    float left_bound = m_initialPosition.x;
+    float next_pos_x = m_position.x + vel_x;
+
+    if ((next_pos_x >= right_bound) || (next_pos_x <= left_bound)) {
+        return true;
+    }
+    return false;
+}
+
+void Minotaur::setFollowDirection()
+{
+    vec2 playerLoc = world.m_player.get_position();
+    if (((playerLoc.x < m_position.x) && (m_velocity.x > 0)) ||
+         ((playerLoc.x > m_position.x) && (m_velocity.x < 0)) ) {
+        // switch direction to follow player if necessary
+        if (direction == Direction::left) {
+            direction = Direction::right;
+        } else {
+            direction = Direction::left;
+        }
+    } 
+}
+
+bool Minotaur::isPlayerClose()
+{
+   vec2 playerLoc = world.m_player.get_position();
+   if (abs(playerLoc.x - m_position.x) <= detectPlayerRange && abs(playerLoc.y - m_position.y) <= detectPlayerRange) {
+       return true;
+   }
+   return false;
 }
 
 void Minotaur::set_bound(float bound) 
@@ -122,4 +195,51 @@ void Minotaur::set_animation()
     if (!isRepeat && tileIndex == 14) is_anim_once = true;
 
     spriteSheet.update_render_data(this, tileIndex);
+}
+
+void Minotaur::initStateTreeMinotaur()
+{
+	vector<Edge> edges = {
+		{idle, running, 1},
+		{idle, jumping, 1},
+		{idle, rising, 1},
+		{idle, falling, 1},
+		{idle, frozen, 1},
+		{idle, dead, 1},
+        {idle, following, 1},
+		{running, idle, 1},
+		{running, jumping, 1},
+		{running, falling, 1},
+		{running, frozen, 1},
+		{running, dead, 1},
+        {running, following, 1},
+		{jumping, rising, 0},
+		{jumping, frozen, 0},
+		{jumping, dead, 0},
+		{rising, idle, 1},
+		{rising, falling, 1},
+		{rising, frozen, 1},
+		{rising, dead, 1},
+		{falling, landing, 1},
+		{falling, frozen, 1},
+		{falling, dead, 1},
+		{landing, idle, 1},
+		{landing, running, 1},
+		{landing, jumping, 1},
+		{landing, frozen, 1},
+		{landing, dead, 1},
+		{frozen, thawing},
+		{thawing, idle, 0},
+		{thawing, running, 0},
+		{thawing, rising, 0},
+		{thawing, falling, 0},
+        {thawing, following, 0},
+        {following, idle, 1},
+        {following, running, 1},
+        {following, frozen, 1},
+		{dead, reviving, 0},
+		{reviving, idle, 0}
+	};
+
+	characterState = new StateTree(edges, idle);
 }
