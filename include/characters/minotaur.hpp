00e966eb77333add19bc9ff270d75939bc0522e1
Julilla Paul <julillapaul@gmail.com>
2019-03-26 21:28:13 -0700

    #124 Base implementation of minotaur

---
+++ include/characters/minotaur.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+#include <stack>
+
+// Simple interface for the minotaur. This is the boss enemy!
+#include "enemy.hpp"
+#include "character.hpp"
+#include "smart.hpp"
+
+class Minotaur: public Smart
+{
+    static Texture minotaur_texture;
+    public:
+        // Creates all the associated render resources and default transform
+        bool init(vec2 initialPosition, Physics* physicsHandler)override;
+
+        // Update player position based on velocity vector
+        // ms represents the number of milliseconds elapsed from the previous update() call
+        void update(float ms)override;
+
+        void draw(const mat3& projection)override;
+
+        void set_animation()override;
+
+        void set_bound(float bound);
+
+    private:
+        float maxVerticalSpeed = 1.f;
+        float maxHorzSpeed = 1.f;
+        float m_stopBound;
+	
+};
