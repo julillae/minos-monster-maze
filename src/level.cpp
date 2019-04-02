7c6c88888dbe693cdff12bb49a8c5660d92f5f6c
hdengg <henry.deng@alumni.ubc.ca>
2019-04-01 23:21:33 -0700

    pass in all floors for boss level

--- src/level.cpp
+++ src/level.cpp
@@ -256,6 +256,12 @@ bool Level::update(float elapsed_ms)
     std::vector<Floor> nearbyFloorComponents =
 			m_quad.getNearbyFloorComponents(m_player.get_position(), m_player.get_bounding_box());
 
+	if (current_level == 11) {
+		// deallocate quad tree memory for boss
+		m_quad.clear();
+		nearbyFloorComponents = m_floors.get_floor_vector();
+	}
+
     // checking player - platform collision
 	check_platform_collisions(nearbyFloorComponents);
 
