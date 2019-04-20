23369a56c69e955c418485262c7e989257bdc535
Yuntian Wan <yuntianwan@dhcp-206-87-156-45.ubcsecure.wireless.ubc.ca>
2019-04-20 16:57:17 -0700

    changed the factor for non-retina display

--- src/level.cpp
+++ src/level.cpp
@@ -440,9 +440,9 @@ void Level::draw()
 	vec2 deviationVector2 = add(p_position, negateVec(cameraCenter));
 	deviationVector2 = rotateVec(deviationVector2, rotation);
 	if (abs(rotation)<2.f && abs(rotation)>0.6f){
-		m_fire.originUpdate(w, h, -deviationVector2.x*2.f, -deviationVector2.y*2.f);
+		m_fire.originUpdate(w, h, -deviationVector2.x, -deviationVector2.y);
 	}else{
-		m_fire.originUpdate(w, h, deviationVector2.x*2.f, -deviationVector2.y*2.f);
+		m_fire.originUpdate(w, h, deviationVector2.x, -deviationVector2.y);
 	}
 	
 	//m_fire.originUpdate(w, h, deviationVector2.x*2.f, -deviationVector2.y*2.f);
